1d9dbbade132de82f897fc919b1e9a25e23a3927
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-01-15 16:36:42 -0600

    created add to collection fuction and album as an object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,12 @@
 console.log('***** Music Collection *****')
 let Collection= [];
+// set Collection as new variable with empty array
+
+function addToCollection (albumTitle, artist, yearPublished){
+    let album = {
+        albumTitle: albumTitle,
+        artist: artist,
+        yearPublished: yearPublished,
+    };
+    
+}
