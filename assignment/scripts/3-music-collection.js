8f807abfaf7bec7af943d65ac6f594e034ca6288
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-01-15 17:44:49 -0600

    Added 6 Albums to Collection Array and tested each function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,5 +8,31 @@ function addToCollection (albumTitle, artist, yearPublished){
         artist: artist,
         yearPublished: yearPublished,
     };
-    
+    Collection.push(album);
+    return album;
 }
+// End of Creating function addToCollection and added Collection to array
+
+// Testing the "addToCollection" Function to add 6 albums
+addToCollection('Thriller' , 'Michael Jackson', 1982);
+console.log('Added Thriller Album' , Collection[0]);// Test 1 Worked
+
+addToCollection('Ecstasy', 'Avant', 2002);
+console.log("Added Ecstasy Album" , Collection[1]);// Test 2 Worked
+
+addToCollection('For The Cool In You', 'Babyface', 1993);
+console.log('Added For The Lover in You Album' , Collection[2]);//Test 3 Worked
+
+addToCollection('Legacy', 'Boyz 2 Men', 1997);
+console.log('Added Legacy Album', Collection[3]);//Test 4 Worked
+
+addToCollection('TrapSoul', 'Bryson Tiller', 2015);
+console.log('Added Trapsoul Album to Collection', Collection[4]);//Test 5 Worked
+
+addToCollection('The Colour of Love', 'Celine Dion', 1993);
+console.log('Added The Colour of Love', Collection[5]);//Test 6 Worked
+
+//Finished adding Albums to Collection all the Functions Worked
+
+
+
